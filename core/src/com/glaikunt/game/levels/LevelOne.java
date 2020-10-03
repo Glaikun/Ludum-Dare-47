dba4902674b4514834c75e5cd6e7dcd05386c8a6
Dregron <Andymurray2007@gmail.com>
2020-10-03 16:06:37 +0100

    Word Phases & Sentence with blank

--- core/src/com/glaikunt/game/levels/LevelOne.java
+++ core/src/com/glaikunt/game/levels/LevelOne.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.glaikunt.application.ApplicationResources;
+import com.glaikunt.application.TickTimer;
 import com.glaikunt.game.phase.Phase;
 import com.glaikunt.game.phase.PhaseOne;
 import com.glaikunt.game.player.PlayerActor;
@@ -13,19 +14,27 @@ public class LevelOne extends Actor {
     private int currentPhase = 0;
     private Phase activePhase;
 
+    private TickTimer winner = new TickTimer(1);
+
     public LevelOne(ApplicationResources applicationResources, PlayerActor player, Stage ux) {
 
         if (currentPhase == 0) {
             this.activePhase = new PhaseOne(applicationResources, player, ux);
             ux.addActor(activePhase);
         }
+
+        winner.setTick(1);
     }
 
     @Override
     public void act(float delta) {
 
         if (activePhase.isPhasePassed()) {
-            Gdx.app.log("DEBUG", "HIIT");
+            winner.tick(delta);
+
+            if (winner.isTimerEventReady()) {
+                Gdx.app.log("DEBUG", "WINNER! WINNER!");
+            }
         }
     }
 }
